16e9b2eb4cbac2dd5f0fee366505b9eb2bff9c87
Marwan Abbas <marwaneltoukhy98@gmail.com>
2025-10-14 09:31:01 -0700

    "autosyncing agent activities"

--- verilog/dv/cocotb/pwm_boundary_test/pwm_boundary_test.c
+++ verilog/dv/cocotb/pwm_boundary_test/pwm_boundary_test.c
@@ -13,8 +13,13 @@ void main() {
     ManagmentGpio_outputEnable();
     ManagmentGpio_write(0);
     
+    enableHkSpi(0);
+    
+    GPIOs_configure(0, GPIO_MODE_USER_STD_OUTPUT);
     GPIOs_loadConfigs();
     User_enableIF();
+    
+    CF_TMR32_setGclkEnable(PWM0_BASE_ADDR, 1);
     
     ManagmentGpio_write(1);
     
@@ -22,10 +27,21 @@ void main() {
     
     for(int i = 0; i < 4; i++) {
         CF_TMR32_disable(PWM0_BASE_ADDR);
-        CF_TMR32_setPrescaler(PWM0_BASE_ADDR, 9);
-        CF_TMR32_setReload(PWM0_BASE_ADDR, 100);
+        
+        CF_TMR32_setPR(PWM0_BASE_ADDR, 9);
+        CF_TMR32_setRELOAD(PWM0_BASE_ADDR, 100);
         CF_TMR32_setCMPX(PWM0_BASE_ADDR, test_cases[i]);
-        CF_TMR32_enablePWM(PWM0_BASE_ADDR);
+        CF_TMR32_setCMPY(PWM0_BASE_ADDR, 200);
+        
+        CF_TMR32_setUpCount(PWM0_BASE_ADDR);
+        CF_TMR32_setPeriodic(PWM0_BASE_ADDR);
+        
+        CF_TMR32_setPWM0MatchingZeroAction(PWM0_BASE_ADDR, CF_TMR32_ACTION_HIGH);
+        CF_TMR32_setPWM0MatchingCMPXUpCountAction(PWM0_BASE_ADDR, CF_TMR32_ACTION_LOW);
+        CF_TMR32_setPWM0MatchingRELOADAction(PWM0_BASE_ADDR, CF_TMR32_ACTION_LOW);
+        
+        CF_TMR32_PWM0Enable(PWM0_BASE_ADDR);
+        
         CF_TMR32_restart(PWM0_BASE_ADDR);
         CF_TMR32_enable(PWM0_BASE_ADDR);
         
