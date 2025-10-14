c4f47f605ac1047c1e91f584ddfdd50fea849b1b
Marwan Abbas <marwaneltoukhy98@gmail.com>
2025-10-14 06:49:43 -0700

    "autosyncing agent activities"

--- analyze_vcd.py
+++ analyze_vcd.py
@@ -114,43 +114,44 @@ if __name__ == '__main__':
     vcd_file = '/workspace/pwm_example/verilog/dv/cocotb/sim/pwm_simple_run4/RTL-pwm_simple_test/waves.vcd'
     
     # Signals to check (in order of signal path)
+    # Hierarchy: caravel_top.uut.chip_core.mprj.*
     signals = {
-        # Wishbone master side (from caravel to user_project_wrapper)
-        'wbs_cyc_i': 'user_project_wrapper.wbs_cyc_i',
-        'wbs_stb_i': 'user_project_wrapper.wbs_stb_i',
-        'wbs_adr_i': 'user_project_wrapper.wbs_adr_i',
-        'wbs_dat_i': 'user_project_wrapper.wbs_dat_i',
-        'wbs_we_i': 'user_project_wrapper.wbs_we_i',
-        'wbs_ack_o': 'user_project_wrapper.wbs_ack_o',
+        # Wishbone master side (from caravel to user_project)
+        'wbs_cyc_i': 'mprj.wbs_cyc_i',
+        'wbs_stb_i': 'mprj.wbs_stb_i',
+        'wbs_adr_i': 'mprj.wbs_adr_i',
+        'wbs_dat_i': 'mprj.wbs_dat_i',
+        'wbs_we_i': 'mprj.wbs_we_i',
+        'wbs_ack_o': 'mprj.wbs_ack_o',
         
         # Bus splitter slave 0 (to CF_TMR32_WB #0)
-        's_wb_cyc_0': 'user_project_wrapper.bus_splitter.s_wb_cyc_0',
-        's_wb_stb_0': 'user_project_wrapper.bus_splitter.s_wb_stb_0',
-        's_wb_adr_0': 'user_project_wrapper.bus_splitter.s_wb_adr_0',
-        's_wb_dat_w_0': 'user_project_wrapper.bus_splitter.s_wb_dat_w_0',
-        's_wb_we_0': 'user_project_wrapper.bus_splitter.s_wb_we_0',
-        's_wb_ack_0': 'user_project_wrapper.bus_splitter.s_wb_ack_0',
+        's_wb_cyc_0': 'bus_splitter_inst.s_wb_cyc_0',
+        's_wb_stb_0': 'bus_splitter_inst.s_wb_stb_0',
+        's_wb_adr_0': 'bus_splitter_inst.s_wb_adr_0',
+        's_wb_dat_w_0': 'bus_splitter_inst.s_wb_dat_w_0',
+        's_wb_we_0': 'bus_splitter_inst.s_wb_we_0',
+        's_wb_ack_0': 'bus_splitter_inst.s_wb_ack_0',
         
         # CF_TMR32_WB #0 internal signals
-        'pwm0_ack': 'user_project_wrapper.pwm0.ack_o',
-        'pwm0_gclk': 'user_project_wrapper.pwm0.GCLK_REG',
-        'pwm0_ctrl': 'user_project_wrapper.pwm0.CTRL_REG',
-        'pwm0_tmr_en': 'user_project_wrapper.pwm0.instance_to_wrap.tmr_en',
-        'pwm0_tmr_start': 'user_project_wrapper.pwm0.instance_to_wrap.tmr_start',
-        'pwm0_pwm0_en': 'user_project_wrapper.pwm0.instance_to_wrap.pwm0_en',
-        'pwm0_tmr': 'user_project_wrapper.pwm0.instance_to_wrap.tmr',
-        'pwm0_out': 'user_project_wrapper.pwm0.pwm0',
+        'pwm0_ack': 'pwm_timer_0.ack_o',
+        'pwm0_gclk': 'pwm_timer_0.GCLK_REG',
+        'pwm0_ctrl': 'pwm_timer_0.CTRL_REG',
+        'pwm0_tmr_en': 'pwm_timer_0.instance_to_wrap.tmr_en',
+        'pwm0_tmr_start': 'pwm_timer_0.instance_to_wrap.tmr_start',
+        'pwm0_pwm0_en': 'pwm_timer_0.instance_to_wrap.pwm0_en',
+        'pwm0_tmr': 'pwm_timer_0.instance_to_wrap.tmr',
+        'pwm0_out': 'pwm_timer_0.pwm0',
         
-        # Output to GPIO
-        'io_out_0': 'user_project_wrapper.io_out[0]',
+        # Output to GPIO (check top-level GPIO signals)
+        'gpio0': 'caravel_top.gpio0',
     }
     
     print("Searching for signals in VCD file...")
     signal_codes = parse_vcd_signals(vcd_file, signals)
     
-    print("\nExtracting signal values around mgmt_gpio=1 time (~1620025ns)...")
-    start_time = 1600000  # Start a bit before configuration completes
-    end_time = 1700000    # End after configuration
+    print("\nExtracting signal values after reset (~50000ns onwards)...")
+    start_time = 50000    # Start after reset
+    end_time = 200000     # Look at first 150us of simulation
     
     values = extract_signal_values(vcd_file, signal_codes, start_time, end_time, max_samples=50)
     
