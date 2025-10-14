34954d48039978536584d4c1812a2b29ac3aa346
Marwan Abbas <marwaneltoukhy98@gmail.com>
2025-10-14 06:14:35 -0700

    "autosyncing agent activities"

--- verilog/dv/cocotb/pwm_test/pwm_test.c
+++ verilog/dv/cocotb/pwm_test/pwm_test.c
@@ -16,12 +16,14 @@ void configurePWM(CF_TMR32_TYPE_PTR pwm, uint32_t prescaler, uint32_t reload, ui
     CF_TMR32_setPR(pwm, prescaler);
     CF_TMR32_setRELOAD(pwm, reload);
     CF_TMR32_setCMPX(pwm, cmpx);
+    CF_TMR32_setCMPY(pwm, 0);
     CF_TMR32_setUpCount(pwm);
     CF_TMR32_setPeriodic(pwm);
     CF_TMR32_setPWM0MatchingZeroAction(pwm, CF_TMR32_ACTION_HIGH);
     CF_TMR32_setPWM0MatchingCMPXUpCountAction(pwm, CF_TMR32_ACTION_LOW);
     CF_TMR32_PWM0Enable(pwm);
     CF_TMR32_enable(pwm);
+    CF_TMR32_restart(pwm);
 }
 
 void main() {
