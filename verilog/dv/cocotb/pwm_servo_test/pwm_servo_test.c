ba573a65184bdb8d653351e1d47c15a4731ff1d5
Marwan Abbas <marwaneltoukhy98@gmail.com>
2025-10-19 03:57:08 -0700

    "autosyncing agent activities"

--- verilog/dv/cocotb/pwm_servo_test/pwm_servo_test.c
+++ verilog/dv/cocotb/pwm_servo_test/pwm_servo_test.c
@@ -84,7 +84,7 @@ void main()
     
     ManagmentGpio_write(1);
     
-    const int _DELAY_VALUE = 1000;
+    const int _DELAY_VALUE = 10;
     
     ManagmentGpio_write(0);
     config_pwm_ticks(18000, 18000, 18000, 18000);
